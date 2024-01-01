8f9940fb47ddfbd2165efbd6fc359f0284068661
Justin Crain <131824461+Jcrain5418@users.noreply.github.com>
2024-01-01 10:14:57 -0600

    project stop point.

--- src/contact_page.js
+++ src/contact_page.js
@@ -1,4 +1,5 @@
 import pizza_outside from './images/pizza_outside.jpg'
+import tokyoMap from './images/tokyo_map.jpeg'
 import './style.css'
 
 export default function contact(){
@@ -11,11 +12,31 @@ export default function contact(){
     header.textContent = "Contact us for delivery or pick up"
     pageContent.appendChild(header)
 
+    const phoneLinkDiv = document.createElement('div')
+    phoneLinkDiv.classList.add('phone_link_div')
+    const phoneNumber = document.createElement('a')
+    phoneNumber.href = 'https://www.youtube.com/watch?v=dQw4w9WgXcQ'
+    phoneNumber.textContent = '817-666-1234'
+    phoneLinkDiv.appendChild(phoneNumber)
+    pageContent.appendChild(phoneLinkDiv)
+
+    const buildingDiv = document.createElement('div')
+    buildingDiv.classList.add('building_image')
     const image = document.createElement('img')
     image.src = pizza_outside
     image.height = '300'
     image.width = '300'
-    pageContent.appendChild(image)
+    buildingDiv.appendChild(image)
+    pageContent.appendChild(buildingDiv)
+
+    const mapDiv = document.createElement('div')
+    mapDiv.classList.add('map_div')
+    const mapImage = document.createElement('img')
+    mapImage.src = tokyoMap
+    mapImage.height = '300'
+    mapImage.width = '300'
+    mapDiv.appendChild(mapImage)
+    pageContent.appendChild(mapDiv)
 
     content.appendChild(pageContent)
 }
